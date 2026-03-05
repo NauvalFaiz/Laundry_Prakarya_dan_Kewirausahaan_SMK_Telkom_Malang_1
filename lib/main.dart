cbcaa6c3246e34c0b717013ee8173316d96218fb
Nauval Faiz Luthf Hisyam <nauvalfaizluthfhisyam33@gmail.com>
2026-03-05 23:06:47 +0700

    Nauval

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 
 void main() {
   runApp(MaterialApp(home: Index(), debugShowCheckedModeBanner: false));
@@ -14,17 +16,98 @@ class Index extends StatelessWidget {
       height: double.infinity,
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Color(0xff0b4b89), Color(0xffFFEC8E), Color(0xffFFFFFF)],
+          colors: [Color(0xff0C4B89), Color(0xffFFFFFF), Color(0xffF7E78E)],
           begin: Alignment.topCenter,
-          stops: [
-            0.0,
-            0.69,
-            1.0
-          ],
+          stops: [0.0, 0.44, 1.0],
           end: Alignment.bottomCenter,
         ),
       ),
-      child: Scaffold(),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Positioned(
+              left: -30,
+              top: -20,
+              child: Image.asset(
+                  width: 289.55,
+                  height: 228,
+                  'assets/atas.png'),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "RajajoWash",
+                    style: GoogleFonts.syne(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  Text(
+                    "Laundry bersih, hidup tenang.",
+                    style: GoogleFonts.plusJakartaSans(fontSize: 14),
+                  ),
+                  SizedBox(height: 115),
+                  Container(
+                    width: 188,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Color(0xff104E89),
+                      boxShadow: [
+                        BoxShadow(color: Color(0xff0D4C89), blurRadius: 15.2),
+                      ],
+                      borderRadius: BorderRadius.circular(57),
+                      border: GradientBoxBorder(
+                        width: 2,
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: [0.0, 0.84],
+                          colors: [Color(0xff0C4B89), Color(0xffF7E790)],
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Get Started",
+                          style: GoogleFonts.plusJakartaSans(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Icon(
+                          Icons.arrow_right_alt,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              right: -30,
+              bottom: -20,
+              child: Image.asset(
+                width: 289.55,
+                height: 228,
+                'assets/bawah.png',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
